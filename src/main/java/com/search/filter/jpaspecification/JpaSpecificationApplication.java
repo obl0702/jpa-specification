e6b8c125e06a6b719a255185cac413d5ccd6198a
boonloong <obl0702@gmail.com>
2023-02-23 15:43:29 +0800

    Initial Version

---
+++ src/main/java/com/search/filter/jpaspecification/JpaSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.search.filter.jpaspecification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpecificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSpecificationApplication.class, args);
+    }
+
+}
